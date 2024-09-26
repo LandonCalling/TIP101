da443a2fa3d5ea8446ddca8d4c1f6d87a09ff67e
Landon Hill <landonmichaelhill@gmail.com>
2024-09-26 16:55:45 -0700

    Completed Best Book problem

--- Unit 2/Session 1/Problem Set 1/best_book.py
+++ Unit 2/Session 1/Problem Set 1/best_book.py
@@ -35,4 +35,74 @@ Expected Output:
  "rating": 4.47
 }
 
+
+Problem:
+Given a list of books, find the book with the highest rating.
+
+- input: list of books
+- output: dictionary representing highest rated book
+- each dictionary is of the form 
+    - {"title": "Title",
+       "author": "Author",
+       "rating": range(?)}
+
+Examples:
+books = [
+    {"title": "Tomorrow, and Tomorrow, and Tomorrow",
+     "author": "Gabrielle Zevin",
+     "rating": 4.18
+    },
+    {"title": "A Fortune For Your Disaster",
+     "author": "Hanif Abdurraqib",
+     "rating": 4.47
+    },
+    {"title": "The Seven Husbands of Evenlyn Hugo",
+     "author": "Taylor Jenkins Reid",
+     "rating": 4.40
+    }
+]
+
+Expected Output:
+
+{"title": "A Fortune For Your Disaster",
+ "author": "Hanif Abdurraqib",
+ "rating": 4.47
+}
+
+Algorithm:
+- remove the first dict(book) from the list
+  and save as the highest rating. 
+- iterate through the remaining list of books 
+  comparing each book's rating with the previous
+  rating.
+    - if the rating is greater, save as highest rating
+- return highest rating
+
 """
+
+def highest_rated(books):
+    highest_rated_book = books[0]
+    books = books[1:]
+    
+    for book in books:
+        if book["rating"] > highest_rated_book["rating"]:
+            highest_rated_book = book
+    
+    return highest_rated_book
+    
+books = [
+    {"title": "Tomorrow, and Tomorrow, and Tomorrow",
+     "author": "Gabrielle Zevin",
+     "rating": 4.18
+    },
+    {"title": "A Fortune For Your Disaster",
+     "author": "Hanif Abdurraqib",
+     "rating": 4.47
+    },
+    {"title": "The Seven Husbands of Evenlyn Hugo",
+     "author": "Taylor Jenkins Reid",
+     "rating": 4.40
+    }
+]
+
+print(highest_rated(books))
